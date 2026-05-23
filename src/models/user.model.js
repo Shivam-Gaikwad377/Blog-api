d3dad8ec36633be5eccc65cb7991db67f7bfa4b3
Shivam Gaikwad <shivamgaikwad.6565@gmail.com>
2026-05-23 22:42:20 +0530

    add jwt tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 import Post from "./post.model.js";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config();
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -32,8 +37,37 @@ const userSchema = new mongoose.Schema({
             ref: "Post",
         },
     ],
+    refreshToken: {
+        type: String,
+    },
 }, { timestamps: true });
 
+userSchema.methods.isValidPassword = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken =  async function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            name: this.name,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
+    )
+}
+userSchema.methods.generateRefreshToken = async function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
+    )
+}
+
 const User = mongoose.model("User", userSchema);
+
 
 export default User;
